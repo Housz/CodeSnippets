262dedcbbced4dd3757c19df49f59320d01ad481
Housz <Housz@Housz.cn>
2023-12-01 10:06:15 +0800

    move

--- CXX/value/test_value.cpp
+++ CXX/value/test_value.cpp
@@ -1,7 +1,24 @@
-
+#include <iostream>
+#include <utility>
+#include <vector>
 
 int main()
 {
+    int &&rr1 = 42; // rr: right reference
+    std::cout << rr1 << std::endl;
+
+    // int &&rr2 = rr1; error, rr1 is left value
+
+    int &&rr3 = std::move(rr1);
+    std::cout << rr3 << std::endl; // 42
+    std::cout << rr1 << std::endl; // 42
+
+    rr3 = 43;
+    std::cout << rr3 << std::endl; // 43
+    std::cout << rr1 << std::endl; // 43
+
+    
+
 
     return 0;
 }
