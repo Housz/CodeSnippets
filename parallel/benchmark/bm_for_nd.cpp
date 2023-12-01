8de2c97b4940db4268bb0ed8d83d23cccdc612b5
Housz <housz@housz.cn>
2023-12-01 21:35:42 +0800

    bench tbb range2d

--- parallel/benchmark/bm_for_nd.cpp
+++ parallel/benchmark/bm_for_nd.cpp
@@ -17,8 +17,163 @@
 #include <algorithm>
 #include <execution>
 #include <vector>
+#include <cmath>
 
-#include <tbb/parallel_for_each.h>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range2d.h>
 #include <benchmark/benchmark.h>
 
+size_t n = 1 << 10;
+std::vector<std::vector<double>> v(n, std::vector<double>(n, 0));
+std::vector<double> v2(n*n);
 
+static void para_for_ij(benchmark::State &state)
+{
+    for (auto _ : state)
+	{
+        tbb::parallel_for(size_t(0), n, [&](size_t i){
+            tbb::parallel_for(size_t(0), n, [&](size_t j){
+                v[i][j] = std::sin(i) * std::cos(j);
+            });
+        });
+    }
+}
+
+static void para_for_ij_v2(benchmark::State &state)
+{
+    for (auto _ : state)
+	{
+        tbb::parallel_for(size_t(0), n, [&](size_t i){
+            tbb::parallel_for(size_t(0), n, [&](size_t j){
+                v2[i*n+j] = std::sin(i) * std::cos(j);
+            });
+        });
+    }
+}
+
+static void para_for_i_v2(benchmark::State &state)
+{
+    for (auto _ : state)
+	{
+        tbb::parallel_for(size_t(0), n*n, [&](size_t i){
+            v2[i] = std::sin(i) * std::cos(i);
+        });
+    }
+}
+
+static void para_for_ji(benchmark::State &state)
+{
+    for (auto _ : state)
+	{
+        tbb::parallel_for(size_t(0), n, [&](size_t j){
+            tbb::parallel_for(size_t(0), n, [&](size_t i){
+                v[i][j] = std::sin(i) * std::cos(j);
+            });
+        });
+    }
+}
+
+static void range2d_1(benchmark::State &state)
+{
+	for (auto _ : state)
+    {
+        tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n),
+        [&](tbb::blocked_range2d<size_t> r){
+            for(size_t i = r.cols().begin(); i < r.cols().end(); i++)
+            {
+                for(size_t j = r.rows().begin(); j < r.rows().end(); j++)
+                {
+                    v[i][j] = std::sin(i) * std::cos(j);
+                }
+            }
+        }
+        );
+    }
+}
+
+static void range2d_1_v2(benchmark::State &state)
+{
+	for (auto _ : state)
+    {
+        tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n),
+        [&](tbb::blocked_range2d<size_t> r){
+            for(size_t i = r.cols().begin(); i < r.cols().end(); i++)
+            {
+                for(size_t j = r.rows().begin(); j < r.rows().end(); j++)
+                {
+                    v2[i*n+j] = std::sin(i) * std::cos(j);
+                }
+            }
+        }
+        );
+    }
+}
+
+static void range2d_2(benchmark::State &state)
+{
+	for (auto _ : state)
+    {
+        tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n),
+        [&](tbb::blocked_range2d<size_t> r){
+            for(size_t i = r.cols().begin(); i < r.cols().end(); i++)
+            {
+                for(size_t j = r.rows().begin(); j < r.rows().end(); j++)
+                {
+                    v[j][i] = std::sin(i) * std::cos(j);
+                }
+            }
+        }
+        );
+    }
+}
+
+
+static void range2d_3(benchmark::State &state)
+{
+	for (auto _ : state)
+    {
+        tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n),
+        [&](tbb::blocked_range2d<size_t> r){
+            for(size_t i = r.rows().begin(); i < r.rows().end(); i++)
+            {
+                for(size_t j = r.cols().begin(); j < r.cols().end(); j++)
+                {
+                    v[i][j] = std::sin(i) * std::cos(j);
+                }
+            }
+        }
+        );
+    }
+}
+
+static void range2d_4(benchmark::State &state)
+{
+	for (auto _ : state)
+    {
+        tbb::parallel_for(tbb::blocked_range2d<size_t>(0, n, 0, n),
+        [&](tbb::blocked_range2d<size_t> r){
+            for(size_t i = r.rows().begin(); i < r.rows().end(); i++)
+            {
+                for(size_t j = r.cols().begin(); j < r.cols().end(); j++)
+                {
+                    v[j][i] = std::sin(i) * std::cos(j);
+                }
+            }
+        }
+        );
+    }
+}
+
+
+
+BENCHMARK(para_for_ij);
+BENCHMARK(para_for_ij_v2);
+BENCHMARK(para_for_i_v2);
+BENCHMARK(para_for_ji);
+BENCHMARK(range2d_1);
+BENCHMARK(range2d_1_v2);
+BENCHMARK(range2d_2);
+BENCHMARK(range2d_3);
+BENCHMARK(range2d_4);
+
+BENCHMARK_MAIN();
