37ecb00c2b4cd3c9a1aa5e01f81ed89562634399
Housz <Housz@Housz.cn>
2024-05-22 15:57:41 +0800

    update parallel reduce avg

--- parallel/testTBB/test_parallel_reduce.cpp
+++ parallel/testTBB/test_parallel_reduce.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 #include <algorithm>
 #include <tbb/blocked_range.h>
@@ -15,7 +16,7 @@
  * 
  * reduction
  */
-int main()
+int main0()
 {
 	size_t n = 11; // sum [sin(0), sin(1), ..., sin(10)] 
 	double res = tbb::parallel_reduce(
@@ -37,6 +38,48 @@ int main()
 	);
 
 	std::cout << res << std::endl;  // 100 + sum [sin(0), sin(1), ..., sin(10)] 
+
+	return 0;
+}
+
+// parallel average
+int main()
+{
+	size_t n = 1 << 26;
+
+	std::vector<float> v(n);
+
+	for (size_t i = 0; i < n; i++)
+	{
+		v[i] = 10.f + (float)std::sin(i);
+	}
+
+	float avg_serial = 0;
+	for (size_t i = 0; i < n; i++)
+	{
+		avg_serial += v[i];
+	}
+	avg_serial /= n;
+	std::cout << "average serial: " << avg_serial << "\n"; // = 4
+
+
+	float avg_parallel = 
+	tbb::parallel_reduce(tbb::blocked_range<size_t>(0, n), (float)0, 
+	[&](tbb::blocked_range<size_t> r, float avg_local){
+		for (size_t i = r.begin(); i != r.end(); i++)
+		{
+			avg_local += v[i];
+		}
+		return avg_local;
+	},
+	// [] (float x, float y) {return x + y;}
+	std::plus<float>()
+	);
+
+	avg_parallel /= n;
+	
+	std::cout << "average parallel: " << avg_parallel << "\n"; // = 10
+
 
 	return 0;
 }
