7d779b0ff8541006e367d2402e84447cbb0fba02
Housz <Housz@Housz.cn>
2024-05-27 15:29:42 +0800

    update bench

--- parallel/benchmark/bm_for_3d.cpp
+++ parallel/benchmark/bm_for_3d.cpp
@@ -2,10 +2,9 @@
 ------------------------------------------------------------
 Benchmark                  Time             CPU   Iterations
 ------------------------------------------------------------
-serial_ijk        3.3715e+10 ns   1.9219e+10 ns            1
-serial_kji        1.3088e+10 ns   7031250000 ns            1
-tbb_blocked3d_kji 2030161400 ns   1515625000 ns            1
-tbb_blocked3d_ijk 4134336500 ns   1406250000 ns            1
+serial_ijk             33704 ms        23422 ms            1
+serial_kji             14923 ms        10562 ms            1
+tbb_blocked3d_ijk       4064 ms         1844 ms            1
 
 */
 
@@ -19,65 +18,58 @@ tbb_blocked3d_ijk 4134336500 ns   1406250000 ns            1
 #include <tbb/blocked_range3d.h>
 #include <benchmark/benchmark.h>
 
+// size_t n = 1 << 10;
 size_t n = 1 << 10;
+size_t X = n;
+size_t Y = n + 1;
+size_t Z = n + 2;
 
-std::vector<double> v(n * n * n);
+// std::vector<double> v(n *n *n);
+std::vector<double> _v(X * Y * Z);
 
-
-static void serial_kji(benchmark::State &state)
+double& v(size_t i, size_t j, size_t k)
 {
-	for (auto _ : state)
+	if (!(i<X && j<Y && k<Z))
 	{
-		for (size_t k = 0; k < n; k++)
-		{
-			for (size_t j = 0; j < n; j++)
-			{
-				for (size_t i = 0; i < n; i++)
-				{
-					v[i + j * n + k * n * n] += sin(i)*cos(i);
-				}
-			}
-		}
+		std::cout << i << " " << j << " " << k << "\n";
 	}
+	
+	return _v[ i + j * X + k * X*Y ];
 }
 
 static void serial_ijk(benchmark::State &state)
 {
 	for (auto _ : state)
 	{
-		for (size_t i = 0; i < n; i++)
+		for (size_t i = 0; i < X; i++)
 		{
-			for (size_t j = 0; j < n; j++)
+			for (size_t j = 0; j < Y; j++)
 			{
-				for (size_t k = 0; k < n; k++)
+				for (size_t k = 0; k < Z; k++)
 				{
-					v[i + j * n + k * n * n] += sin(i)*cos(i);
+					// v[i + j * n + k * n * n] += sin(i) * cos(i);
+					v(i, j, k) = sin(i) * cos(i);
 				}
 			}
 		}
 	}
 }
 
-
-static void tbb_blocked3d_kji(benchmark::State &state)
+static void serial_kji(benchmark::State &state)
 {
 	for (auto _ : state)
 	{
-		tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, 0, n, 0, n),
-			[&](tbb::blocked_range3d<size_t> r){
-				for (size_t k = r.pages().begin(); k < r.pages().end(); k++)
+		for (size_t k = 0; k < Z; k++)
+		{
+			for (size_t j = 0; j < Y; j++)
+			{
+				for (size_t i = 0; i < X; i++)
 				{
-					for (size_t j = r.cols().begin(); j < r.cols().end(); j++)
-					{
-						for (size_t i = r.rows().begin(); i < r.rows().end(); i++)
-						{
-							v[i + j * n + k * n * n] += sin(i)*cos(i);
-						}
-					}
+					// v[i + j * n + k * n * n] += sin(i) * cos(i);
+					v(i, j, k) = sin(i) * cos(i);
 				}
-				
 			}
-		);
+		}
 	}
 }
 
@@ -85,27 +77,51 @@ static void tbb_blocked3d_ijk(benchmark::State &state)
 {
 	for (auto _ : state)
 	{
-		tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, 0, n, 0, n),
-			[&](tbb::blocked_range3d<size_t> r){
-				for (size_t i = r.pages().begin(); i < r.pages().end(); i++)
-				{
-					for (size_t j = r.cols().begin(); j < r.cols().end(); j++)
-					{
-						for (size_t k = r.rows().begin(); k < r.rows().end(); k++)
-						{
-							v[i + j * n + k * n * n] += sin(i)*cos(i);
-						}
-					}
-				}
-				
-			}
-		);
+		tbb::parallel_for(tbb::blocked_range3d<size_t>(0, X, 0, Y, 0, Z),
+						  [&](tbb::blocked_range3d<size_t> r)
+						  {
+							  for (size_t i = r.pages().begin(); i < r.pages().end(); i++)
+							  {
+								  for (size_t j = r.rows().begin(); j < r.rows().end(); j++)
+								  {
+									  for (size_t k = r.cols().begin(); k < r.cols().end(); k++)
+									  {
+										//   v[i + j * n + k * n * n] += sin(i) * cos(i);
+										  v(i, j, k) = sin(i) * cos(i);
+									  }
+								  }
+							  }
+						  });
 	}
 }
 
+// err
+static void tbb_blocked3d_kji(benchmark::State &state)
+{
+	for (auto _ : state)
+	{
+		tbb::parallel_for(tbb::blocked_range3d<size_t>(0, Z, 0, Y, 0, X),
+						  [&](tbb::blocked_range3d<size_t> r)
+						  {
+							  for (size_t k = r.cols().begin(); k < r.cols().end(); k++)
+							  {
+								  for (size_t j = r.rows().begin(); j < r.rows().end(); j++)
+								  {
+									  for (size_t i = r.pages().begin(); i < r.pages().end(); i++)
+									  {
+										//   v[i + j * n + k * n * n] += sin(i) * cos(i);
+										  v(i, j, k) = sin(i) * cos(i);
+									  }
+								  }
+							  }
+						  });
+	}
+}
+
+
 BENCHMARK(serial_ijk);
 BENCHMARK(serial_kji);
-BENCHMARK(tbb_blocked3d_kji);
 BENCHMARK(tbb_blocked3d_ijk);
+// BENCHMARK(tbb_blocked3d_kji);
 
 BENCHMARK_MAIN();
